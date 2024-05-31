50118d286533da030d4462250e979b733498e162
Dominik308 <sauterdominik1211@gmail.com>
2024-05-31 09:39:46 +0200

    init

---
+++ test.py
@@ -0,0 +1,11 @@
+import time
+
+print("Skript gestartet")
+i = 0
+
+while True:  # If script is not done, container is still on
+    print(i)
+    i += 1
+    time.sleep(2)
+
+print("Skript vorbei = Container aus!!!")
