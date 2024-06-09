cee324b2b12f74c63550c80fb6962459bc4abc2b
Padinator <dieser.patrick@gmail.com>
2024-06-09 19:16:04 +0200

    - Works with all docker containers

--- fetcher_service/publisher_service.py
+++ fetcher_service/publisher_service.py
@@ -1,22 +1,28 @@
 import time
+import requests as r
+import subprocess
 
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-# Asynchronous by default
+def get_ip_of_broker(name: str) -> str:
+    ip = subprocess.run('ping -c1 broker | head -n1 | cut -d" " -f3', shell=True, stdout=subprocess.PIPE)
+    return ip.stdout.decode('utf-8')[1:-2]
+
+
+time.sleep(10)  # Wait for ending creation of broker
+ip_of_broker = get_ip_of_broker("broker")
+producer = KafkaProducer(bootstrap_servers=f'{ip_of_broker}:19092')
 future = producer.send('my-topic', b'raw_bytes')
 
-# Block for 'synchronous' sends
-while True:
-    try:
-        record_metadata = future.get(timeout=5)
-        producer.flush()
-        print(record_metadata)
-    except KafkaError as ex:
-        # Decide what to do if produce request failed...
-        print(ex)
-        # exit()
-    finally:
-        time.sleep(3)
+try:
+    record_metadata = future.get(timeout=5)
+    producer.flush()
+    print(record_metadata)
+except KafkaError as ex:
+    # Decide what to do if produce request failed...
+    print(ex)
+    # exit()
+finally:
+    time.sleep(3)
--- processor_service/consumer_service.py
+++ processor_service/consumer_service.py
@@ -1,13 +1,24 @@
+import subprocess
+import time
+
 from kafka import KafkaConsumer
 
-while True:
-    consumer = KafkaConsumer('my-topic',
-                             bootstrap_servers='localhost:9092',
-                             auto_offset_reset='earliest',
-                             consumer_timeout_ms=1000)
-    for message in consumer:
-        # message value and key are raw bytes -- decode if necessary!
-        # e.g., for unicode: `message.value.decode('utf-8')`
-        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                              message.offset, message.key,
-                                              message.value))
+
+def get_ip_of_broker(name: str) -> str:
+    ip = subprocess.run('ping -c1 broker | head -n1 | cut -d" " -f3', shell=True, stdout=subprocess.PIPE)
+    return ip.stdout.decode('utf-8')[1:-2]
+
+
+time.sleep(10)  # Wait for ending creation of broker
+ip_of_broker = get_ip_of_broker("broker")
+consumer = KafkaConsumer('my-topic',
+                         bootstrap_servers=f'{ip_of_broker}:19092',
+                         auto_offset_reset='earliest',
+                         consumer_timeout_ms=1000)
+
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
