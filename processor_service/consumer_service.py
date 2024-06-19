3ce13212d790c1a67d03f6fbd8f1db7cffb441a6
Dominik308 <sauterdominik1211@gmail.com>
2024-06-19 16:39:27 +0200

    Not working properly

--- processor_service/consumer_service.py
+++ processor_service/consumer_service.py
@@ -17,33 +17,32 @@ def clean_data(data: str) -> pd.DataFrame:
     """Function to clean and restructure stock market data"""
     if isinstance(data, str):
         data = json.loads(data)
-    
+
     cleaned_data = []
-    
+
     timestamps = list(data['Open'].keys())
-    
+
     for timestamp in timestamps:
         row = {'Timestamp': pd.to_datetime(int(timestamp), unit='ms').strftime('%Y-%m-%d %H:%M:%S')}
         for key in ['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits']:
             row[key] = data[key].get(timestamp, 0)
         cleaned_data.append(row)
-    
+
     df = pd.DataFrame(cleaned_data)
     df.sort_values('Timestamp', inplace=True)
-    
+
     print("Successfully cleaned the data!")
-    
+
     return df
-
-
 
 # Wait for ending creation of broker
 time.sleep(10)
 
 # Create a Kafka consumer
 ip_of_broker = get_ip_of_broker("broker")
+print(ip_of_broker)
 consumer = KafkaConsumer(
-    *['stonks_1y', 'stonks_1mo', 'stonks_1wk', 'real_time'],  # List of topics
+    *['stonks_1y', 'stonks_1mo', 'stonks_5d', 'real_time'],  # List of topics
     bootstrap_servers=f'{ip_of_broker}:19092',
     auto_offset_reset='earliest',
     value_deserializer=lambda v: json.loads(v.decode('utf-8'))  # Deserializer function
@@ -52,7 +51,7 @@ consumer = KafkaConsumer(
 # Create an Elasticsearch client
 ip_of_data_preparer = get_ip_of_broker("data-preparer")
 es = Elasticsearch(
-    hosts=[{"host": "host.docker.internal", "port": 9200}]
+    hosts=[{"host": "host.docker.internal", "port": 9200, "scheme": "http"}]
     # [f'http://{ip_of_data_preparer}:9200'],
     # basic_auth=("elastic", "MagicWord")
 )
@@ -78,14 +77,18 @@ es.indices.put_settings(
 for message in consumer:
     # Get the raw data
     raw_data = message.value
-    print("RAW DATA", raw_data)
+    print("RAW DATA", raw_data, flush=True)
 
-    # Clean the data
-    cleaned_data = clean_data(raw_data)
+    # Check the topic of the message
+    if message.topic == 'real_time':
+        # For real-time data, just send it to Elasticsearch
+        es.index(index='real_time', body=raw_data)
+    else:
+        # For historical data, clean it and convert it to a DataFrame
+        cleaned_data = clean_data(raw_data)
+        # Convert the cleaned data to a dictionary
+        cleaned_data_dict = cleaned_data.to_dict(orient='records')
 
-    # Convert the cleaned data to a dictionary
-    cleaned_data_dict = cleaned_data.to_dict(orient='records')
-
-    # Send the cleaned data to Elasticsearch
-    for record in cleaned_data_dict:
-        es.index(index='stock_data', body=record)
+        # Send the cleaned data to Elasticsearch
+        for record in cleaned_data_dict:
+            es.index(index='stock_data', body=record)
