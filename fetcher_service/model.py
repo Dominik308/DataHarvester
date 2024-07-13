0e3e959f027e1a6e967e20b9d8cd9b1a524e1f88
Tom Lauth <66142861+lauthtom@users.noreply.github.com>
2024-07-13 12:28:12 +0200

    Better Model for predicting Stock Values

--- fetcher_service/model.py
+++ fetcher_service/model.py
@@ -1,95 +1,101 @@
+import matplotlib.pyplot as plt
 import yfinance as yf
 import numpy as np
-import pandas as pd
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
-from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score
 
-def create_dataset(dataset, time_step: int) -> tuple[np.array:, np.array]:
-    """Create datasets: "time_step many data" to predict next data (e.g. 60er packets)"""
-    data_x, data_y = [], []
+# Get Stock Data from API
+df = yf.Ticker("AAPL").history(period="1y")
+df['Average'] = (df['High'] + df['Low'] + df['Close'] + df['Open']) / 4
+df = df[["Average"]]
 
-    for i in range(len(dataset) - time_step - 1):
-        a = dataset[i:(i + time_step), 0]
-        data_x.append(a)
-        data_y.append(dataset[i + time_step, 0])
+# Little optimum for Apple
+last_values = 55
 
-    return np.array(data_x), np.array(data_y)
+# Get data
+changes = np.diff(np.array(df.values).ravel())
+all_x_and_y = [(changes[i:i + last_values], (changes[i + last_values] > 0).astype(int)) for i in  # TODO: sum better!?!?
+               range(changes.shape[0] - last_values)]
 
+changes, target_labels = np.array([x for x, _ in all_x_and_y]), np.array([y for _, y in all_x_and_y])
 
-# Parameter
-time_step = 120
+# Split Data in Training- and Validationsets
+X_train, X_test, y_train, y_test = train_test_split(changes, target_labels, test_size=0.2, shuffle=False, stratify=None)
 
-# 1. Get data
-ticker = 'AAPL'
-data = yf.download(ticker, start='2019-07-01', end='2024-07-01')
-data = data[['Close']]
+# Create and configure Logistic Regression
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train)
 
-# 2. Prepare data
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(data)
+# Prediction
+y_pred = log_reg.predict(X_test)
 
-# Create data sets
-X, y = create_dataset(scaled_data, time_step)
+# Accuracy of the prediction
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
 
-# Split data into train data and test data
-train_size = int(len(X) * 0.8)
-test_size = len(X) - train_size
-train_X, test_X = X[0:train_size], X[train_size:len(X)]
-train_y, test_y = y[0:train_size], y[train_size:len(y)]
-
-# Reshape data for LSTM [samples, time steps, features]
-train_X = train_X.reshape(train_X.shape[0], train_X.shape[1], 1)
-test_X = test_X.reshape(test_X.shape[0], test_X.shape[1], 1)
-
-# 3. Create model
-model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(time_step, 1)))
-model.add(LSTM(50, return_sequences=False))
-model.add(Dense(25))
-model.add(Dense(1))
-
-# Compile model with square loss and adam
-model.compile(optimizer='adam', loss='mean_squared_error')
-
-# Train model with train data
-model.fit(train_X, train_y, batch_size=1, epochs=10)
-
-# Make predictions for last time
-train_predict = model.predict(train_X)
-test_predict = model.predict(test_X)
-
-# Scale data back (back from intial preparing data)
-train_predict = scaler.inverse_transform(train_predict)
-test_predict = scaler.inverse_transform(test_predict)
-
-# Plot predictions
-plt.figure(figsize=(14, 5))
-plt.plot(data.index, data["Close"])
-plt.plot(data.index[-test_predict.shape[0]:], test_predict, label='Testing Data Prediction')
+# Plot Data of the prediction
+fig, ax = plt.subplots(nrows=2, ncols=1)
+ax[0].plot(np.arange(X_train.shape[0]), y_train)
+ax[0].plot(np.arange(X_train.shape[0], X_train.shape[0] + X_test.shape[0]), y_test)
+ax[0].plot(np.arange(X_train.shape[0], X_train.shape[0] + X_test.shape[0]), y_pred)
+ax[1].plot(np.arange(df.shape[0] - last_values - 1), df.values[last_values + 1:])
 plt.legend()
 plt.show()
 
-# Make predictions for next 30 days
-count_of_next_days = 120
-last_days = scaled_data[-time_step:]
-next_days_prediction = []
+# Prediction of next absolute difference
+changes = np.abs(np.diff(np.array(df.values).ravel()))
+all_x_and_y = [(changes[i:i + last_values], changes[i + last_values]) for i in
+               range(changes.shape[0] - last_values)]  # Labels: next change
 
-for _ in range(count_of_next_days):
-    last_days = last_days.reshape(1, time_step, 1)
-    predicted_price = model.predict(last_days)
-    next_days_prediction.append(predicted_price[0, 0])
-    last_days = np.append(last_days[0, 1:], predicted_price, axis=0)
+changes, target_labels = np.array([x for x, _ in all_x_and_y]), np.array([y for _, y in all_x_and_y])
 
-next_days_prediction = scaler.inverse_transform(np.array(next_days_prediction).reshape(-1, 1))
+# Split Data in Training- and Validationsets
+X_train_change, X_test_change, y_train_change, y_test_change = train_test_split(changes, target_labels, test_size=0.2, shuffle=False, stratify=None)
 
-# Plot predictions
-plt.figure(figsize=(14, 5))
-plt.plot(range(len(data)), data['Close'], label='Original Data')
-plt.plot(range(len(data), len(data) + count_of_next_days), next_days_prediction,
-         label=f'Next {count_of_next_days} Days Prediction', color='red')
+# Create and Configure Random Forest Regressor
+rf_reg = RandomForestRegressor(n_estimators=100, random_state=42)
+rf_reg.fit(X_train_change, y_train_change)
+
+# Prediction of the change
+predicted_changes = rf_reg.predict(X_test_change)
+
+# Plot of actual and predicted changes
+fig, ax = plt.subplots(nrows=2, ncols=1)
+ax[0].plot(np.arange(X_train_change.shape[0]), y_train_change, color="royalblue")
+ax[0].plot(np.arange(X_train_change.shape[0], X_train_change.shape[0] + X_test_change.shape[0]), y_test_change, color="orange")
+ax[0].plot(np.arange(X_train_change.shape[0], X_train_change.shape[0] + X_test_change.shape[0]), predicted_changes, color="green")
+ax[1].plot(np.arange(df.shape[0] - last_values - 1), df.values[last_values + 1:])
+plt.legend()
+plt.show()
+
+# Combination of both models = predict next value
+# Prediction of the next 30 Values
+num_predictions = 30
+last_prices = np.array(df.values[-last_values - 1:]).ravel()  # Calc with last 55 changes/diffs = last 56 values
+predictions = []
+
+for i in range(num_predictions):
+    changes = np.array([np.diff(last_prices).ravel()])
+
+    # Prediction of direction and magnitude
+    direction = log_reg.predict(changes)[0]
+    magnitude = rf_reg.predict(np.abs(changes))[0]
+
+    if direction == 1:
+        next_price = last_prices[-1] + magnitude
+    else:
+        next_price = last_prices[-1] - magnitude
+
+    predictions.append(next_price)
+    last_prices = np.append(last_prices[1:], next_price)
+
+# Plot of historic Data and Prediction
+plt.figure(figsize=(10, 6))
+plt.plot(np.arange(df.shape[0]), df.values, label='Historic Data')
+plt.plot(np.arange(df.shape[0], df.shape[0] + num_predictions), predictions, label='Predicted Values',
+         color='red')
 plt.legend()
 plt.show()
