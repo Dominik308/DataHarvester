af5deafea131ef81bf49caee2ee1dec8707a85a3
Padinator <dieser.patrick@gmail.com>
2024-07-16 12:11:01 +0200

    - Changed time format

--- producer_service/producer_service.py
+++ producer_service/producer_service.py
@@ -43,7 +43,7 @@ def send_stonk_data(stonk: str) -> None:
             data = {
                 'symbol': stock.info['symbol'],
                 'price': frame_of_day['average'],
-                'timestamp': date.strftime('%Y-%m-%d') + " 00:00:00"
+                'date': date.timestamp()
             }
             producer.send(f'{stonk}_{period}', data)
             producer.flush()
